e0960fc04f82614adb5e03f83401275901b6559a
liuk123 <liu___k@outlook.com>
2020-06-29 10:52:57 +0800

    分布打包

--- build/gen-style.js
+++ build/gen-style.js
@@ -3,6 +3,7 @@ const cleanCSS = require('gulp-clean-css');
 const less = require('gulp-less');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
+const components = require('./components.json');
 
 function buildCss(cb) {
   gulp.src('../styles/index.less')
@@ -14,4 +15,17 @@ function buildCss(cb) {
   cb()
 }
 
-exports.default = gulp.series(buildCss);
+function buildSeperateCss(cb) {
+  Object.keys(components).forEach(compName => {
+    gulp.src(`../styles/${compName}.less`)
+      .pipe(less())
+      .pipe(autoprefixer())
+      .pipe(cleanCSS())
+      .pipe(rename(`${compName}.css`))
+      .pipe(gulp.dest('../lib/styles'));
+  })
+
+  cb()
+}
+
+exports.default = gulp.series(buildCss, buildSeperateCss);
